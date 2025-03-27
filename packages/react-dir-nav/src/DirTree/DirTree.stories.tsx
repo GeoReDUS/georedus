306c16f3c092de89ead051fa7f375a7e19493b9f
Simon Fan <simon@orioro.design>
2025-03-27 14:07:00 -0300

    chore: add DirTree large tree story

--- packages/react-dir-nav/src/DirTree/DirTree.stories.tsx
+++ packages/react-dir-nav/src/DirTree/DirTree.stories.tsx
@@ -8,6 +8,45 @@ export default {
 
 const NODES = []
 
+function generateTestNodes(count = 500) {
+  const nodes = []
+  let root = 1
+  let branch = 1
+  let subBranch = 1
+
+  for (let i = 0; i < count; i++) {
+    let parts = []
+
+    // Add Root
+    parts.push(`Root ${root}`)
+
+    // Add Branch
+    if (i % 2 === 0) {
+      parts.push(`Branch ${root}${branch}`)
+    }
+
+    // Add Sub-Branch
+    if (i % 3 === 0) {
+      parts.push(`Branch ${root}${branch}${subBranch}`)
+    }
+
+    nodes.push(parts.join(' / '))
+
+    // Update counters to keep variety
+    subBranch++
+    if (subBranch > 3) {
+      subBranch = 1
+      branch++
+      if (branch > 5) {
+        branch = 1
+        root++
+      }
+    }
+  }
+
+  return nodes
+}
+
 const DirTree = makeDirTree()
 
 export const Basic = () => {
@@ -20,6 +59,17 @@ export const Basic = () => {
         'Root 2 / Branch 21',
         'Root 2 / Branch 22 / 23',
       ]).map((node) => ({
+        ...node,
+        type: 'dir',
+      }))}
+    />
+  )
+}
+
+export const LargeTree = () => {
+  return (
+    <DirTree
+      tree={nodesFromPaths(generateTestNodes(2_000)).map((node) => ({
         ...node,
         type: 'dir',
       }))}
