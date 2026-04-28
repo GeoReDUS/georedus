3a8bf1be0f997b9c22162143d330565b470a1324
Caroline <carolinecaleguer@gmail.com>
2026-04-28 17:39:17 -0300

    feat: style panel implementation in vector_line

--- packages/georedus-ui/src/viewSpecs/presets/vector_line/index.jsx
+++ packages/georedus-ui/src/viewSpecs/presets/vector_line/index.jsx
@@ -4,6 +4,13 @@ import { Z_OVERLAY_BASE_1000, Z_OVERLAY_MIDDLE_2000 } from '../../zIndexes'
 import { interpolate } from '@orioro/util'
 import { resolve } from '@orioro/resolve'
 import { resolveColor } from '../../util'
+import { Flex } from '@orioro/react-ui-core'
+import { COLOR_OPTIONS } from '../color_options/index'
+
+const LINE_PATTERN_OPTIONS = [
+  { label: 'Continua', value: 'line' },
+  { label: 'Tracejada', value: 'dashed' },
+]
 
 function _parseTiles(tiles, context) {
   tiles = Array.isArray(tiles)
@@ -37,24 +44,99 @@ export function vector_line(
     throw new Error('source_layer must be defined')
   }
 
-  const _color = resolveColor(color)
-  const _line_pattern = {
-    legendItemProps: {
-      box: {
-        style: {
-          height: 0,
-          borderColor: _color,
-          borderStyle:
-            line.paint && line.paint['line-dasharray'] ? 'dashed' : 'line',
-          borderWidth: '1px',
+  const _initialColor = resolveColor(color)
+  const _color = resolve.fn(
+    (ctx) => resolveColor(ctx.view?.conf?.style?.color) || _initialColor,
+  )
+
+  const _initialLinePattern =
+    line.paint && line.paint['line-dasharray'] ? 'dashed' : 'line'
+
+  const _linePattern = resolve.fn(_color, (_resolvedColor, ctx) => {
+    const resolvedLinePattern =
+      ctx.view?.conf?.style?.linePattern || _initialLinePattern
+
+    return {
+      'line-width': 1,
+      'line-color': _color,
+      ...(line.paint || {}),
+      ...(resolvedLinePattern === 'dashed' ? { 'line-dasharray': [2, 2] } : {}),
+    }
+  })
+
+  const _legend = resolve.fn(_color, (_resolvedColor, ctx) => {
+    return {
+      type: 'CategoricalLegend',
+      items: [
+        {
+          label,
+          box: {
+            style: {
+              height: 0,
+              borderColor: _resolvedColor,
+              borderStyle:
+                ctx.view?.conf?.style?.linePattern || _initialLinePattern,
+              borderWidth: '1px',
+            },
+          },
         },
-      },
-    },
-  }
+      ],
+    }
+  })
 
   return {
     ...props,
     label,
+    confSchema: {
+      style: {
+        color: {
+          label: 'Cor',
+          helperText: 'Selecione a cor para a camada',
+          type: 'select',
+          clearable: false,
+          defaultValue: _initialColor,
+          options: COLOR_OPTIONS.map((opt) => ({
+            ...opt,
+            label: (
+              <Flex direction="row" alignItems="center" gap="2">
+                <div
+                  style={{
+                    width: '15px',
+                    height: '15px',
+                    backgroundColor: opt.value,
+                  }}
+                />
+                <div>{opt.label}</div>
+              </Flex>
+            ),
+          })),
+        },
+        linePattern: {
+          label: 'Linha',
+          type: 'select',
+          clearable: false,
+          defaultValue: _initialLinePattern || 'line',
+          options: LINE_PATTERN_OPTIONS.map((opt) => {
+            return {
+              ...opt,
+              label: (
+                <Flex direction="row" alignItems="center" gap="2">
+                  <div
+                    style={{
+                      width: 15,
+                      height: 0,
+                      borderStyle: opt.value === 'line' ? 'solid' : 'dashed',
+                      borderWidth: 1,
+                    }}
+                  />
+                  <div> {opt.label}</div>
+                </Flex>
+              ),
+            }
+          }),
+        },
+      },
+    },
     metadata: {},
     sources: {
       main: {
@@ -71,22 +153,8 @@ export function vector_line(
         'source-layer': source_layer,
         type: 'line',
         ...line,
-        paint: {
-          'line-width': 1,
-          'line-color': _color,
-          ...(line.paint || {}),
-        },
-        legends: [
-          {
-            type: 'CategoricalLegend',
-            items: [
-              {
-                label,
-                ..._line_pattern.legendItemProps,
-              },
-            ],
-          },
-        ],
+        paint: _linePattern,
+        legends: [_legend],
         tooltip: {
           title: [
             '$literal',
