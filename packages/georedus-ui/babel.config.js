c76d289341206cdf63a63d4456a480ce84b54d03
Simon Fan <simon@orioro.design>
2026-05-27 00:25:31 -0300

    fix: fix react babel preset to use {runtime: automatic}

--- packages/georedus-ui/babel.config.js
+++ packages/georedus-ui/babel.config.js
@@ -2,6 +2,6 @@
 // storybook requires babel.config.js instead of babel.config.mjs
 // due to async loading requirements
 //
-import { BABEL_PRESET } from '@orioro/dev/react'
+import { babelConfig } from '@orioro/dev/react'
 
-export default BABEL_PRESET
+export default babelConfig()
