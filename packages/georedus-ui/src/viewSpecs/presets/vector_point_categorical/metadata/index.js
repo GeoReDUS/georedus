d558608ad116f8044cbdb2c817e215e4df9c4a29
Caroline <carolinecaleguer@gmail.com>
2026-06-19 16:30:58 -0300

    feat: created vector_point_categorical  preset

---
+++ packages/georedus-ui/src/viewSpecs/presets/vector_point_categorical/metadata/index.js
@@ -0,0 +1,69 @@
+import { interpolate } from '@orioro/util'
+import { resolveAsync } from '@orioro/resolve'
+import { humanize } from '../../util'
+import { uniqBy } from 'lodash'
+import { resolveSchemeColor } from '../../../util'
+
+export function metadata(viewSpec, allViewSpecs, context) {
+  const { style } = viewSpec
+
+  const categories = resolveAsync.fn(async (ctx) => {
+    // Resolve color scheme
+    const colorSchemeId =
+      ctx.view?.conf?.style?.colorScheme || 'schemeGeoReDUSSafe'
+
+    // resolve categories
+    const resolvedCategories =
+      typeof style.categories === 'string'
+        ? // style.categories is an URL
+          await fetch(
+            interpolate(style.categories, {
+              METADATA_API_ENDPOINT: context.METADATA_API_ENDPOINT,
+            }),
+          )
+            .then((res) => res.json())
+            .then((features) => {
+              const categoryKey = style.categoryKey
+              const uniqueValues = uniqBy(
+                features
+                  .map((feature) => ({
+                    value: feature[categoryKey],
+                  }))
+                  .filter((cat) => cat.value != null),
+                'value',
+              )
+              return uniqueValues
+            })
+        : Array.isArray(style.categories)
+          ? style.categories.map((categoryInput) =>
+              typeof categoryInput === 'string'
+                ? {
+                    value: categoryInput,
+                  }
+                : categoryInput,
+            )
+          : null
+
+    if (!resolvedCategories) {
+      throw new Error('could not resolve categories ' + viewSpec.id)
+    }
+
+    return resolvedCategories.map((cat, index) => {
+      const color = cat.color || resolveSchemeColor(colorSchemeId, index)
+
+      if (!color) {
+        throw new Error(`Could not resolve color for ${cat.value}`)
+      }
+
+      return {
+        ...cat,
+        color,
+        label: cat.label || humanize(cat.value),
+      }
+    })
+  })
+
+  return {
+    categories,
+  }
+}
