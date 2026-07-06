563375ad6dc22ab6172c3f368a3f5cc7437b69c8
Caroline <carolinecaleguer@gmail.com>
2026-07-06 18:32:50 -0300

    fix: removed unused code

--- packages/georedus-ui/src/viewSpecs/presets/vector_polygon_continuous/confSchema.jsx
+++ packages/georedus-ui/src/viewSpecs/presets/vector_polygon_continuous/confSchema.jsx
@@ -1,43 +1,5 @@
-import { Flex } from '@orioro/react-ui-core'
 import { DEFAULT_COLOR_SCHEME_ID } from './parseStyleSpec'
-import { D3_DIVERGING, D3_SEQUENTIAL } from '../../util'
 import { schemeSelector } from '../util/components/confInputs'
-
-const getLargestColorArray = (scheme) => {
-  return scheme[scheme.length - 1]
-}
-
-const D3_SCHEMES = {
-  ...D3_SEQUENTIAL,
-  ...D3_DIVERGING,
-}
-
-const COLOR_SCHEME_OPTIONS = Object.entries(D3_SCHEMES).map(
-  ([name, scheme]) => {
-    const colors = getLargestColorArray(scheme)
-
-    return {
-      value: name,
-      label: (
-        <Flex direction="row" gap="2" alignItems="center">
-          <div style={{ display: 'flex', gap: '2px' }}>
-            {colors.map((color, idx) => (
-              <div
-                key={idx}
-                style={{
-                  width: 15,
-                  height: 15,
-                  backgroundColor: color,
-                  border: 'none',
-                }}
-              />
-            ))}
-          </div>
-        </Flex>
-      ),
-    }
-  },
-)
 
 export function confSchema(viewSpec, allViewSpecs, context) {
   return {
