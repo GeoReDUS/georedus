0646802214011dab69899d8f4d523d7c2d564eb0
Caroline <carolinecaleguer@gmail.com>
2026-08-19 11:00:11 -0300

    feat: added coment to municipio filter

--- packages/georedus-ui/src/viewSpecs/presets/util/municipioFilter.ts
+++ packages/georedus-ui/src/viewSpecs/presets/util/municipioFilter.ts
@@ -1,12 +1,15 @@
-// export function municipioFilter(municipioId: number) {
-//   return ['all', ['==', ['get', 'cd_mun'], municipioId]]
-// }
-
-
 import { resolve } from '@orioro/resolve'
 
 export function municipioFilter() {
   return resolve.fn(({ app }) =>
-    ['all', app.regional ? null : ['==', ['get', 'cd_mun'], app.municipioId]].filter(Boolean),
+    [
+      'all',
+      app.regional ? null : ['==', ['get', 'cd_mun'], app.municipioId],
+    ].filter(Boolean),
   )
 }
+
+// Filtro que não considera visualização de dados regionais
+// export function municipioFilter(municipioId: number) {
+//   return ['all', ['==', ['get', 'cd_mun'], municipioId]]
+// }
